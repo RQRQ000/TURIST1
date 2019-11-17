e8cfeb47d7e525095afafa415028c1d81495cbe0
user <userer>
2019-11-17 13:19:57 +0700

    123

--- app/src/main/java/com/example/turist/MainActivity.kt
+++ app/src/main/java/com/example/turist/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.example.turist
 
+import `values-en`.Sights
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+  //  var pi: Sights = Sights("Нулевой километр", R.drawable.nulevoykilometr, resources.getString(R.string.kilohistory))
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
     }
 }
---
+++ app/src/main/java/com/example/turist/Sights.kt
@@ -0,0 +1,11 @@
+package `values-en`
+
+import android.graphics.PointF
+
+class Sights(name : String, picture : Int, history : String) {
+    var onmap : PointF = PointF()
+    var history : String = history
+    var picture : Int = picture
+    var name : String = name
+}
+
