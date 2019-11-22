cdba6093facc1ef1cc8a488506f02ca65fa46b00
user <userer>
2019-11-22 11:06:31 +0700

    123

--- app/src/main/java/com/example/turist/Main3Activity.kt
+++ app/src/main/java/com/example/turist/Main3Activity.kt
@@ -2,11 +2,31 @@ package com.example.turist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.SpinnerAdapter
+import kotlinx.android.synthetic.main.activity_main3.*
 
 class Main3Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+        val adapter: ArrayAdapter<String> = ArrayAdapter(this, R.layout.activity_main3)
+        adapter.setDropDownViewResource(android.R.layout.activity_list_item)
+        spinner.adapter= adapter
+        spinner.onItemSelectedListener = object : AdapterView.OnItemClickListener,
+            AdapterView.OnItemSelectedListener {
+            override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+            }
+        }
     }
 }
