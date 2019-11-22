d76ace082e46564e1d6454b82eb26c697e900852
user <userer>
2019-11-22 09:29:22 +0700

    123

--- app/src/main/java/com/example/turist/Main3Activity.kt
+++ app/src/main/java/com/example/turist/Main3Activity.kt
@@ -2,13 +2,11 @@ package com.example.turist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 
-class Main2Activity : AppCompatActivity() {
-
+class Main3Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_main3)
     }
 }
