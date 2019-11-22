a29ccd4d2bde75fbcd344c5b4e935040571d0a5b
user <userer>
2019-11-22 09:18:48 +0700

    123

--- app/src/main/java/com/example/turist/MainActivity.kt
+++ app/src/main/java/com/example/turist/MainActivity.kt
@@ -7,11 +7,11 @@ import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-  //  var pi: Sights = Sights("Нулевой километр", R.drawable.nulevoykilometr, resources.getString(R.string.kilohistory))
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_main)
+
     }
 }
