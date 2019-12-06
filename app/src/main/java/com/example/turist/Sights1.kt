2ad6f958caae5b94e504d38150e03d20b5989064
mariklolik <marshelo44@gmail.com>
2019-12-06 09:54:33 +0700

    a

--- app/src/main/java/com/example/turist/Sights1.kt
+++ app/src/main/java/com/example/turist/Sights1.kt
@@ -4,4 +4,5 @@ import `values-en`.Sights
 
 object Sights1 {
     val mass : MutableList<Sights> = mutableListOf()
+     var picture: Array<String> = arrayOf( R.drawable.nulevoykilometr,R.drawable.demidovskystolp,R.drawable.corolevavaz,R.drawable.vasaizyashmy,R.drawable.,R.drawable.pamatnystolp,R.drawable.ybileynayavasa,R.drawable.vasakolivanskixmasterov,R.drawable.florenskaymmozayka,R.drawable.yistokovvelikoyreki,R.drawable.molodostaltay,R.drawable.mazaykaozeromoxovoe,R.drawable.mozaykapodvodnymir)
 }
